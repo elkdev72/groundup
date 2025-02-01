ff044cceceb9c1c3ffbf228012618eb406a42fe7
elkdev72 <elijahkanyia.developer@gmail.com>
2025-02-02 01:05:49 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# PYTHON CASTING
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+# GET TYPEOF
+print(type(x))
+print(type(y))
+print(type(z))
+
+# ASSIGN MULTIPLE VARIABLES 
+oranges,banana,cherry = "oranges","banana","cherry"
+print(oranges)
+print(banana)
+print(cherry)
+
+# UNPACKING A COLLECTION
+fruits = ["apple","banana","oranges"]
+x,y,z = fruits
+print(x,y,z)
+
+# GLOBAL VARS
+# local var 
+def myfunc():
+    x = "fantastic"
+
+    print("Python is " + x)
+
+myfunc()
+print(x) #Gives "apple" which is outside the function
+
+# GLOBAL VAR
+def myfunctg():
+    global g
+    g = "fantastic"
+myfunctg()
+
+print("Python is " + g)
+
+
+#Random Number
+import random
+print(random.randrange(10,100))
+
+# Multiline Strings
+a =""""
+Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.
+"""
+print(a)
